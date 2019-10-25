063eb4e51db8cde225f7ecfa1e2d35b60d69e5d9
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-10-25 19:57:33 +0900

    ログイン、ログアウト機能を実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,24 @@
 class SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: session_params[:email].downcase)
+
+    if user&.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      redirect_to posts_path, notice: 'ログインしました'
+    else
+      # flash.now[:danger] = 'ログインに失敗しました'
+      redirect_to new_session_path, notice: '名前とパスワードが一致しません'
+    end
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_path, notice: 'ログアウトしました'
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root 'tops#index'
   resources :users
+
   resources :posts do
     collection do
       post :confirm
     end
   end
+
+  resources :sessions, only: [:new, :create, :destroy]
 end
