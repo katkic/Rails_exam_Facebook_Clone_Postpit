f15ea61ce3c2ac0abba6056cfb4522ed184e1ade
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-10-28 22:35:21 +0900

    画面のレイアウトを修正

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,18 +2,39 @@ class AccountsController < ApplicationController
   before_action :login_required
 
   def show
-    set_user
+    @user = current_user
   end
 
   def edit
+    @user = current_user
   end
 
   def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to account_path, notice: 'アカウント情報を更新しました'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = current_user
+    @user.destroy
+    redirect_to root_path, notice: 'アカウントを削除しました'
   end
 end
 
 private
 
-def set_user
-  @user = User.find(current_user.id)
+def user_params
+  params.require(:user).permit(
+    :last_name,
+    :first_name,
+    :email,
+    :birthday,
+    :sex,
+    :password,
+    :password_confirmation
+  )
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   skip_before_action :login_required
-
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
