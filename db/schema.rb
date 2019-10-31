f9545fbc74a39214c1e4632654161e278be98347
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-10-31 22:21:56 +0900

    usersテーブルにユーザーアイコンのカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_25_034238) do
+ActiveRecord::Schema.define(version: 2019_10_30_133442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2019_10_25_034238) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "image"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
