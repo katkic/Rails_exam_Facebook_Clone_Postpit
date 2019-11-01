59b32db1efd8f034384346849b06e4007bbc0cc5
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-11-01 21:16:06 +0900

    投稿本文と写真にバリデーションを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :user
+
+  validates :content, presence: true, length: { maximum: 255 }
+  validates :image, presence: true
 end
