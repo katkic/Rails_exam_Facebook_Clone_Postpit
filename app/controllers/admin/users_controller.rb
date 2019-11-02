cc108e824e02fa37e7d16428b8741d402aebc29b
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-11-02 23:13:59 +0900

    管理者用のユーザー登録機能を実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,13 +1,44 @@
 class Admin::UsersController < ApplicationController
-  def new
-  end
-
-  def edit
+  def index
   end
 
   def show
   end
 
-  def index
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_user_path(@user),
+          notice: "ユーザー「#{@user.last_name + ' ' + @user.first_name}」を登録しました"
+    else
+      render :new
+    end
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(
+      :last_name,
+      :first_name,
+      :email,
+      :birthday,
+      :sex,
+      :password,
+      :password_confirmation,
+      :image
+    )
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
