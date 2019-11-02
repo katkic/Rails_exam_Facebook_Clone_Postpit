f1550eb2232115ff329c66a056f3fa83979d9a95
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-11-02 10:12:47 +0900

    ActiveRecordのタイムゾーンを日本時間に設定

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module FakebookAppSample
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.time_zone = 'Asia/Tokyo'
+    config.active_record.default_timezone = :local
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
