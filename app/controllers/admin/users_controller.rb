53917c9653d1dab026e879536b2d2e8f84e2252d
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-11-03 21:40:29 +0900

    ユーザー一覧表示をid昇順で表示するよう変更

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,7 @@ class Admin::UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
-    @users = User.all
+    @users = User.order(id: :asc)
   end
 
   def show
